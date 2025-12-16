39b57993f7c5e02cae6fac98d055d9f53a62ab83
Timon Gemberg-Wiesike <gembergwiesike@outlook.de>
2025-12-17 00:30:22 +0100

    create particles

--- Particles_life/game.py
+++ Particles_life/game.py
@@ -148,3 +148,20 @@ def calculate_forces(grid, interaction_matrix, noise_param, r_max):
                         
             total_forces[target_idx] = gesamtkraft
     return total_forces
+
+import numpy as np
+
+letter_index = {'Blau': 0, 'Rot': 1, 'Gelb': 2, 'Grün': 3}
+
+def init_particles(n, width, height):
+    # Zufällige Positionen
+    pos = np.random.rand(n, 2) * np.array([width, height])
+    
+    # Zufällige Start-Geschwindigkeiten (optional, hier 0)
+    vel = np.zeros((n, 2))
+    
+    # Zufällige Typen (A, B, C)
+    type_keys = list(letter_index.keys())
+    types = np.random.choice(type_keys, n)
+    
+    return pos, vel, types
